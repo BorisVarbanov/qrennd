ef753e7ca1938e4f823b4d807082917c74cbfdc6
Boris Varbanov <b.m.varbanov@gmail.com>
2022-06-15 23:23:42 +0200

    Create config.py
    Create a config dataclass.

---
+++ qrennd/utils/config.py
@@ -0,0 +1,51 @@
+"""Parameter configuration (Config) class."""
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Type, TypeVar
+
+import yaml
+
+T = TypeVar("T", bound="Config")
+
+
+@dataclass
+class Config:
+    """Config class containing data, train and model hyperparameters and checkpoint/summary directories."""
+
+    exp_name: str
+    data: dict
+    train: dict
+    model: dict
+    summary_dir: Path = field(repr=False, init=False)
+    checkpoint_dir: Path = field(repr=False, init=False)
+
+    def __post_init__(self) -> None:
+        """Generate the summary and checkpoints directories."""
+        experiments_dir = Path("../experiments")
+        self.summary_dir = experiments_dir / self.exp_name / "summary"
+        self.checkpoint_dir = experiments_dir / self.exp_name / "checkpoint"
+
+    @classmethod
+    def from_yaml(cls: Type[T], filename: str) -> T:
+        """
+        from_yaml Create new qrennd.utils.Config instance from YAML configuarion file.
+
+        Parameters
+        ----------
+        filename : str
+            The YAML file name.
+
+        Returns
+        -------
+        T
+            The initialised qrennd.utils.Config object based on the yaml.
+        """
+        with open(filename, "r") as file:
+            setup = yaml.safe_load(file)
+
+        exp_name = setup.get("exp_name")
+        data = setup.get("data")
+        train = setup.get("train")
+        model = setup.get("model")
+
+        return cls(exp_name, data, train, model)
