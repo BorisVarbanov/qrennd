e35e8cf78c82f06a00a76f5bef546503441cd19c
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-26 12:22:45 +0100

    Update run_training.sh
    Add conda activation

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -12,15 +12,18 @@
 #SBATCH --mail-user=b.m.varbanov@tudelft.nl
 #SBATCH --mail-type=END,FAIL
 
-
 module load 2022r2
 module load openmpi
 module load cuda/11.1.1
 module load cudnn/8.0.5.39-11.1
 module load miniconda3
 
+unset CONDA_SHLVL
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
+conda activate qrennd-gpu-env
 srun python train_qrennd.py
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
