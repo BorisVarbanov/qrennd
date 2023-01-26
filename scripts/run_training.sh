e89699113afec6180bc82f12dd6ec832fe539393
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-26 12:20:07 +0100

    Update run_training.sh
    Update train script parameters

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,21 +1,26 @@
 #!/bin/bash -l
 #
-#SBATCH --job-name="qrennd-decoder-training"
+#SBATCH --job-name="qrennd-train-test"
 #SBATCH --partition=gpu
-#SBATCH --time=01:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G
-#SBATCH --output=/scratch/bmvarbanov/.log/surf17/s17_%j.out
-#SBATCH --error=/scratch/bmvarbanov/.log/surf17/s17_%j.err
+#SBATCH --output=/scratch/bmvarbanov/.log/qrennd/train_%j.out
+#SBATCH --error=/scratch/bmvarbanov/.log/qrennd/train_%j.err
 #SBATCH --mail-user=b.m.varbanov@tudelft.nl
 #SBATCH --mail-type=END,FAIL
 
 
 module load 2022r2
-module load python
 module load openmpi
-module load py-tensorflow
+module load cuda/11.1.1
+module load cudnn/8.0.5.39-11.1
+module load miniconda3
+
+previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
 srun python train_qrennd.py
+
+/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
