f614dab65579493e1c2bcb9bd765b80d1193747d
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-27 18:58:13 +0100

    Update __init__.py
    Fix module imports.

--- qrennd/__init__.py
+++ qrennd/__init__.py
@@ -1,7 +1,9 @@
 """Main qrennd module."""
 __version__ = "0.1.0"
 
+from . import layouts, utils
+from .layouts import Layout
 from .models import get_model
 from .utils import Config
 
-__all__ = ["get_model", "Config"]
+__all__ = ["get_model", "layouts", "utils", "Config", "Layout"]
