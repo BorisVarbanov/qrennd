b167e8a35e6c7ee4bf21f46036c8811944fcfacd
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-28 14:08:07 +0100

    Update run_training.sh
    Update slurm execution to new script version

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -2,26 +2,23 @@
 #
 #SBATCH --job-name="qrennd-train-test"
 #SBATCH --partition=gpu
-#SBATCH --time=05:00:00
+#SBATCH --time=02:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=3G
 #SBATCH --mail-user=b.m.varbanov@tudelft.nl
 #SBATCH --mail-type=END,FAIL
+#SBATCH --output=train_qrennd.%j.out
+#SBATCH --error=train_qrennd.%j.err
 
 module load 2022r2
+module load python
 module load openmpi
-module load cuda/11.1.1
-module load cudnn/8.0.5.39-11.1
-module load miniconda3
-
-unset CONDA_SHLVL
-source "$(conda info --base)/etc/profile.d/conda.sh"
+module load py-tensorflow
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
-conda activate qrennd-gpu-env
-srun python train_qrennd.py
+srun --mpi=pmix python train_qrennd.py
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
