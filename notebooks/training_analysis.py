f032994f3ea59426e7ae58a645fa724bc93d0812
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-02-07 12:24:27 +0100

    Add MWPM line in accuracy

--- notebooks/training_analysis.py
+++ notebooks/training_analysis.py
@@ -5,34 +5,70 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # %%
-NOTEBOOK_DIR = pathlib.Path.cwd()
-
-DATA_DIR = NOTEBOOK_DIR / "data"
-if not DATA_DIR.exists():
-    raise ValueError("Data directory not found")
+EXP_NAME = "training_logs"
+MODEL_FOLDER: str = "20230206-114805-base_training_no-dropout_no-l2"
 
 # %%
-LOG_FILE = "training.log"
-dataframe = pd.read_csv(DATA_DIR / LOG_FILE)
+NOTEBOOK_DIR = pathlib.Path.cwd()  # define the path where the notebook is placed.
+
+# experiment folder
+EXP_DIR = NOTEBOOK_DIR / "data" / EXP_NAME
+if not EXP_DIR.exists():
+    raise ValueError("Experiment directory does not exist.")
+
+LOG_FILE = EXP_DIR / f"{MODEL_FOLDER}/logs/training.log"
+if not LOG_FILE.exists():
+    raise ValueError(f"Log file does not exist: {MODEL_FOLDER}/logs/training.log")
+
+# %%
+dataframe = pd.read_csv(LOG_FILE)
 
 # %%
 dataframe
 
 # %%
-metrics = ("loss", "main_output_accuracy")
+METRICS = ("loss", "main_output_accuracy")
+EPOCH_CUT = 100
+acc_MWPM = 0.72482
 
-fig, axs = plt.subplots(figsize=(10, 4), ncols=2)
-for ax, metric in zip(axs, metrics):
+for metric in METRICS:
+    fig, axs = plt.subplots(figsize=(10, 4), ncols=2)
 
-    ax.plot(dataframe.epoch, dataframe[metric], color="blue", label="Training")
-
-    ax.plot(
-        dataframe.epoch, dataframe["val_" + metric], color="orange", label="Validation"
+    axs[0].plot(
+        dataframe.epoch, dataframe[metric], ".-", color="blue", label="Training"
+    )
+    axs[0].plot(
+        dataframe.epoch,
+        dataframe["val_" + metric],
+        ".-",
+        color="orange",
+        label="Validation",
     )
 
-    ax.legend(frameon=False)
-    ax.set_xlabel("Epochs")
-    ax.set_ylabel(metric.replace("_", " ").capitalize())
+    axs[1].plot(
+        dataframe.epoch[EPOCH_CUT:],
+        dataframe[metric][EPOCH_CUT:],
+        ".-",
+        color="blue",
+        label="Training",
+    )
+    axs[1].plot(
+        dataframe.epoch[EPOCH_CUT:],
+        dataframe["val_" + metric][EPOCH_CUT:],
+        ".-",
+        color="orange",
+        label="Validation",
+    )
+
+    if metric == "main_output_accuracy":
+        axs[0].axhline(y=acc_MWPM, linestyle="--", color="gray", label="MWPM (test)")
+
+    axs[0].legend(frameon=False)
+    axs[0].set_xlabel("Epochs")
+    axs[0].set_ylabel(metric.replace("_", " ").capitalize())
+    axs[1].legend(frameon=False)
+    axs[1].set_xlabel("Epochs")
+    axs[1].set_xlim(EPOCH_CUT, max(dataframe.epoch) + 1)
 
 plt.show()
 
