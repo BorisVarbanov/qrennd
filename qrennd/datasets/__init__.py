ec4b652559552bb2a264e1be8044bf02bc157a96
Boris Varbanov <b.m.varbanov@gmail.com>
2023-02-27 19:27:28 +0100

    Fix imports

--- qrennd/datasets/__init__.py
+++ qrennd/datasets/__init__.py
@@ -1,10 +1,12 @@
-from .generators import DataGenerator, DataGeneratorGoogle
-from .util import load_datasets, load_datasets_google
 from . import preprocessing
+from .generators import dataset_genereator
+from .util import load_datasets
+from .sequences import RaggedSequence
+
 
 __all__ = [
-    "DataGenerator",
-    "DataGeneratorGoogle",
+    "RaggedSequence",
+    "dataset_genereator",
     "load_datasets",
     "load_datasets_google",
     "preprocessing",
