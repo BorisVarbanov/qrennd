a6ddf62d47840ed38f8f48c71ae2e34ee58ada10
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-04-21 16:48:58 +0200

    Add first version

--- notebooks/plotting_analysis.py
+++ notebooks/plotting_analysis.py
@@ -1,10 +1,12 @@
 # %%
 import pathlib
+import copy
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import xarray as xr
+import yaml
 
 from qrennd.utils.analysis import (
     logical_fidelity,
@@ -13,51 +15,64 @@ from qrennd.utils.analysis import (
 )
 
 # %%
-EXP_NAME = pathlib.Path("20230418-d3_simulated_google_20M")
-DATASETS = [
-    EXP_NAME / "20230419-224354_best-config_center_7_5" / "test.nc",
-    EXP_NAME / "20230419-224354_best-config_center_7_5" / "test_exp.nc",
-]
-COLORS = ["blue", "red", "orange", "green"]
-LABELS = [
-    "sim",
-    "exp",
-    "center_3_5",
-    "center_5_3",
-]
+TEMPLATE = "nn_vs_pymatching.yaml"
 
-ERRORBARS = False
-MIN_QEC_FIT = 3
+# %%
+PATH_TEMPLATES = pathlib.Path.cwd() / "plot_templates"
+with open(PATH_TEMPLATES / TEMPLATE) as file:
+    setup = yaml.full_load(file)
 
-OUTPUT_FOLDER = EXP_NAME / "20230419-224354_best-config_center_7_5"
-OUTPUT_NAME = "comparison_sim-exp"
 
-TITLE = None
+def format(string: str, formatter: dict) -> str:
+    if string:
+        return string.format(**formatter)
+    return
+
 
 # %%
 DIR = pathlib.Path.cwd() / "output"
 
-fig, ax = plt.subplots()
+NON_DATASETS = ["description", "vars", "figure", "output_name", "output_dir"]
+datasets = [v for k, v in setup.items() if k not in NON_DATASETS]
+variables = setup.get("vars")
 
-for dataset, color, label in zip(DATASETS, COLORS, LABELS):
-    log_fid = xr.load_dataset(DIR / dataset)
+# %%
+fig, ax = plt.subplots(figsize=(7, 5))
+
+for dataset in datasets:
+    log_fid = xr.load_dataset(DIR / format(dataset["dataset"], variables))
     x, y = log_fid.qec_round.values, log_fid.avg.values
-    yerr = log_fid.err.values if ERRORBARS else 0
-    ax.errorbar(
-        x, y, yerr=yerr, fmt=".", color=color, markersize=10, capsize=2, label=label
-    )
+    if data := dataset.get("data"):
+        yerr = log_fid.err.values if data.get("errorbar") else 0
+        ax.errorbar(
+            x,
+            y,
+            yerr=yerr,
+            fmt=data.get("fmt"),
+            color=data.get("color"),
+            markersize=10,
+            capsize=2,
+            label=format(data.get("label"), variables),
+        )
 
-    model_decay = LogicalFidelityDecay(fixed_t0=False)
-    params = model_decay.guess(y, x=x)
-    out = model_decay.fit(y, params, x=x, min_qec=MIN_QEC_FIT)
-    error_rate = lmfit_par_to_ufloat(out.params["error_rate"])
-    t0 = lmfit_par_to_ufloat(out.params["t0"])
+    if fit := dataset.get("fit"):
+        model_decay = LogicalFidelityDecay(fixed_t0=fit.get("fixed_t0"))
+        params = model_decay.guess(y, x=x)
+        out = model_decay.fit(y, params, x=x, min_qec=fit.get("min_qec"))
+        error_rate = lmfit_par_to_ufloat(out.params["error_rate"])
+        t0 = lmfit_par_to_ufloat(out.params["t0"])
 
-    x_fit = np.linspace(MIN_QEC_FIT, max(x), 100)
-    y_fit = model_decay.func(x_fit, error_rate.nominal_value, t0.nominal_value)
-    ax.plot(
-        x_fit, y_fit, "-", color=color, label=f"$\\epsilon_L = (${error_rate*100})%"
-    )
+        x_fit = np.linspace(fit.get("min_qec"), max(x), 100)
+        y_fit = model_decay.func(x_fit, error_rate.nominal_value, t0.nominal_value)
+        vars_fit = {"error_rate": error_rate, "t0": t0}
+        vars_fit.update(variables)
+        ax.plot(
+            x_fit,
+            y_fit,
+            linestyle=fit.get("fmt"),
+            color=fit.get("color"),
+            label=format(fit.get("label"), vars_fit),
+        )
 
 ax.set_xlabel("QEC round")
 ax.set_ylabel("logical fidelity")
@@ -68,9 +83,18 @@ ax.set_yticks(
 )
 ax.legend(loc="best")
 ax.grid(which="major")
-ax.set_title(TITLE)
-fig = ax.get_figure()
+ax.set_title(format(setup["figure"].get("title"), variables))
 fig.tight_layout()
-fig.savefig(DIR / OUTPUT_FOLDER / f"{OUTPUT_NAME}.pdf", format="pdf")
-fig.savefig(DIR / OUTPUT_FOLDER / f"{OUTPUT_NAME}.png", format="png")
+fig.savefig(
+    DIR
+    / format(setup["output_dir"], variables)
+    / format(setup["output_name"] + ".pdf", variables),
+    format="pdf",
+)
+fig.savefig(
+    DIR
+    / format(setup["output_dir"], variables)
+    / format(setup["output_name"] + ".png", variables),
+    format="png",
+)
 plt.show()
